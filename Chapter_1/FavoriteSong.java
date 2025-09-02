97765aa9731c73482ce45886fc324dc3058ba458
LloydAnthonyLangamin-UCM <king.mamaw789@gmail.com>
2025-09-02 14:02:20 +0800

    Add files via upload

---
+++ Chapter_1/FavoriteSong.java
@@ -0,0 +1,15 @@
+public class FavoriteSong {
+    public static void main(String[] args) {
+        String[] songLines = {
+            "Stairway to Heaven by Led Zeppelin",
+            "Mundo by IV OF SPADES",
+            "OZONE by Unique Salonga",
+            "Burnout by SugarFree",
+            "Huling Sayaw by Kamikazee"
+        };
+
+        for (int i = 0; i < songLines.length; i++) {
+            System.out.println(songLines[i]);
+        }
+    }
+}
