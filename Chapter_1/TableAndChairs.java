1ac527105c4aa29edc0fb423669f80edc2755649
LloydAnthonyLangamin-UCM <king.mamaw789@gmail.com>
2025-09-02 13:54:23 +0800

    Add files via upload

---
+++ Chapter_1/TableAndChairs.java
@@ -0,0 +1,17 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        int height = 10; 
+
+        for (int i = 0; i < 6; i++) {
+            System.out.print("        *                                             *        \n");
+        }
+        
+        for (int i = 0; i < 1; i++) {
+            System.out.println("        * * * * *    # # # # # # # # # # #    * * * * *        ");
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            System.out.println("        *        *             #             *        *        ");
+        }
+    }
+}
