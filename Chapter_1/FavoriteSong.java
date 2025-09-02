f998b27a48d29849ae8744df7d1c29a01f839e4a
LloydAnthonyLangamin-UCM <king.mamaw789@gmail.com>
2025-09-02 19:44:09 +0800

    Update FavoriteSong.java

--- Chapter_1/FavoriteSong.java
+++ Chapter_1/FavoriteSong.java
@@ -1,15 +1,15 @@
 public class FavoriteSong {
     public static void main(String[] args) {
         String[] songLines = {
-            "Stairway to Heaven by Led Zeppelin",
-            "Mundo by IV OF SPADES",
-            "OZONE by Unique Salonga",
-            "Burnout by SugarFree",
-            "Huling Sayaw by Kamikazee"
+            "Sunugin natin ang ating mga paa",
+            "Dagitab ng bawat padyak ay nadarama",
+            "Sunugin natin ang ating mga paa",
+            "Dagitab ng bawat padyak ay nadarama"
         };
 
         for (int i = 0; i < songLines.length; i++) {
             System.out.println(songLines[i]);
         }
     }
+
 }
