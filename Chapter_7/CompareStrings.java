6d39f2a8bee9ce839a237bfd9e24f6e0940b9705
LloydAnthonyLangamin-UCM <king.mamaw789@gmail.com>
2025-11-25 22:43:42 +0800

    Update CompareStrings.java

--- Chapter_7/CompareStrings.java
+++ Chapter_7/CompareStrings.java
@@ -1,24 +1,22 @@
 import java.util.Scanner;
 
-public class CompareStrings {
-
+class CompareStrings {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your name >");
+        String name = input.next();
+        StringBuilder sb = new StringBuilder(name);
 
-        final String PREDEFINED_STRING = "Carmen";
-
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.print("Enter your name > ");
-        String userInput = scanner.nextLine();
-
-        if (userInput.equals(PREDEFINED_STRING)) {
-
-            System.out.println(PREDEFINED_STRING + " equals " + userInput);
+        if (sb == null || sb.isEmpty()) {
+            System.out.println();
         } else {
+            String result = name.substring(0, 1).toUpperCase() + name.substring(1);
 
-            System.out.println(PREDEFINED_STRING + " does not equal " + userInput);
+            if (sb.toString().equals(result)) {
+                System.out.println(result + " is equal to " + sb);
+            } else {
+                System.out.println(result + " is not equal to " + sb);
+            }
         }
-
-        scanner.close();
     }
 }
